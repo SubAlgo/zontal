52b8aa717d3acd77d120f820d0218b9da0116fdf
Kornchaval <kornchaval@gmail.com>
2018-10-29 23:36:04 +0700

    Edit menu for relate user permissin

--- layouts/header.php
+++ layouts/header.php
@@ -1,23 +1,32 @@
-<div>Header</div>
+<div class="container-fluid text-right" style="background-color:#ef9292; color:white; margin-bottom:0px; ">
+    <span>Email: pongsathon.jong@bumail.com <br>Support: 0979944942</span>
+</div>
 
-<div class="container-fluid text-right" style="margin-right: 5px;">
-    <?php
-        if(isset($_SESSION['email'])) {
-            echo ("Email : {$_SESSION['email']} <br>");
-        }
-        
-        if(isset($_SESSION['permission'])) {
-            switch($_SESSION['permission']) {
-                case 1 :
-                    echo "Status : Admin";
-                    break;
-                case 2 :
-                    echo "Status : Teacher";
-                    break;
-                case 3 :
-                    echo "Status : Student";
-                    break;
+<div class="container-fluid row" style="background-color:#db6262; height:100px; margin:0px; color:white;">
+    <div class="col-md-3" style="margin:auto;">
+        ZONTAL + 
+    </div>
+
+    <div class="col-md-9 text-right" style="margin:auto;">
+        <?php
+            if(isset($_SESSION['email'])) {
+                echo ("Email : {$_SESSION['email']} <br>");
             }
-        }
-    ?>
+
+            if(isset($_SESSION['permission'])) {
+                switch($_SESSION['permission']) {
+                    case 1 :
+                        echo "Status : Admin";
+                        break;
+                    case 2 :
+                        echo "Status : Teacher";
+                        break;
+                    case 3 :
+                        echo "Status : Student";
+                        break;
+                }
+            }
+        ?>
+    </div>
+
 </div>
--- layouts/menu.php
+++ layouts/menu.php
@@ -1,4 +1,23 @@
-<div style="border: 1px solid black">
+<style>
+    a:link {
+    text-decoration: none;
+    color: white;
+    }
+
+    a:visited {
+        text-decoration: none;
+        color: white;
+    }
+
+    a:hover {
+        background-color:#ff6666;
+    }
+
+    a:active {
+
+    }
+</style>
+<div style="border: 1px solid black; background-color:#3d3c3c">
     <a href="http://localhost/zontal/">Home</a>|
     <a href="http://localhost/zontal/student_register.php">Student_Register</a>|
     <a href="http://localhost/zontal/teacher_register.php">Teacher_Register</a>|
@@ -9,9 +28,40 @@
     <a href="http://localhost/zontal/login.php">Login</a>|
     <a href="http://localhost/zontal/logout.php">Logout</a>
 </div>
-<div style="border: 1px solid black">
+<div style="border: 1px solid black; background-color:#3d3c3c">
     <a href="http://localhost/zontal/student_join_class.php">Student join class</a>|
     <a href="http://localhost/zontal/generate.php">Generate</a>|
     <!--<a href="http://localhost/zontal/class_search.php">Class Search</a>|-->
     
 </div>
+
+
+
+<div class="container-fluid text-center" style="border: 1px solid black; background-color:#3d3c3c; color:white;">
+
+
+<?php
+    if( (!isset($_SESSION['email'])) && (!isset($_SESSION['permission'])) ) {
+        echo("<a href='http://localhost/zontal/login.php'>Login</a> | ");
+        echo("<a href='http://localhost/zontal/student_register.php'>Student_Register</a> | ");
+        echo("<a href='http://localhost/zontal/teacher_register.php'>Teacher_Register</a> | ");
+    }
+
+    if( (isset($_SESSION['email'])) && (isset($_SESSION['permission'])) ) {
+        if($_SESSION['permission'] == 2) {
+            echo("<a href='http://localhost/zontal/'>Teacher Daskboard</a> | ");
+            echo("<a href='http://localhost/zontal/group_setting.php'>Create_Group</a> | ");
+            echo("<a href='http://localhost/zontal/logout.php'>Logout</a>");
+        }
+
+        if($_SESSION['permission'] == 3) {
+            echo("<a href='http://localhost/zontal/student_daskboard.php'>Student Daskboard</a> | ");
+            echo("<a href='http://localhost/zontal/student_join_class.php'>join class</a> | ");
+            echo("<a href='http://localhost/zontal/vaktest.php'>VAK Test</a> | ");
+            echo("<a href='http://localhost/zontal/logout.php'>Logout</a>");
+        }
+    }
+?>
+</div>
+
+
