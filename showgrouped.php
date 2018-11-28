0bdafe23c32688b736fec783506b4d47e4fd67e5
Kornchaval <kornchaval@gmail.com>
2018-11-28 21:44:05 +0700

    std Dashboard show result grouped

---
+++ showgrouped.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    include('config.php');
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- import bootstrap and JS -->
+    <?php
+        include("./layouts/meta.php");
+    ?>
+    <!--
+    
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    -->
+    <title>แสดงผลลัพธ์การจัดกลุ่ม</title>
+</head>
+<body>
+    <?php include('./layouts/header.php'); ?>
+    <?php include('./layouts/menu.php'); ?>
+
+    <!-- content -->
+    <div>
+        <?php
+            /** DATA **/
+            $classId = ($_GET['c_id']);
+            $email = $_SESSION['email'];
+            $name = checkName($conn, $email);
+            $data_decode = getData($conn, $classId);
+            $group;
+
+        ?>
+
+        <?php
+            /** FUNCTION **/
+            function checkName($conn, $email) {
+                $sql = "SELECT users.name FROM users WHERE users.email = '{$email}'";
+                $result = mysqli_query($conn, $sql);
+                while($row = $result->fetch_assoc()) {
+                    $data = $row['name'];
+                }
+                return $data;
+            }
+
+            function getData($conn, $classId) {
+                $sql = "SELECT result from result_grouped where classid = '{$classId}'";
+
+                $result = mysqli_query($conn, $sql);
+                $data;
+                if (mysqli_num_rows($result) > 0) {
+                    while($row = $result->fetch_assoc()) {
+
+                        $data = $row['result'];
+                    
+                        echo("<br>");
+                    } 
+                } else {
+                    $arr = array("error");
+                    $myJSON = json_encode($arr);
+                    echo $myJSON;
+                }
+                $data_decode = json_decode($data);
+
+                return $data_decode;
+            }
+        ?>
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h4>ผลลัพธ์การจัดกลุ่ม</h4>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-2">
+                </div>
+                <div class="col-md-8"  style="border: 1px solid;">
+                    <table class="table table-striped">
+                        <thead class="text-center">
+                            <tr>
+                                <td><b>กลุ่มที่</b></td>
+                                <td><b>สมาชิก</b></td>
+                            </tr>
+                        </thead>
+                        <tbody class="text-center">
+                            <?php
+                                $i = 1;
+                                foreach($data_decode as $val) {
+                                    echo("<tr>");
+                                        echo("<td>{$i}</td>");
+                                        $i++;
+
+                                        echo("<td>");
+                                            foreach($val as $v) {
+                                                if($v->name != $name) {
+                                                    echo($v->name);
+                                                } else {
+                                                    echo("<b><font color='blue'>{$v->name}</font></b>");
+                                                    $group = $i;
+                                                }
+                                                echo(" | ");
+                                            }
+                                        echo("</td>");
+
+                                    echo("</tr>");
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+
+
+                </div>
+                <div class="col-md-2">
+                </div>
+            
+            </div>
+
+            <div class="row text-center">
+                <div class="col-md-12">
+                    <?php
+                        $group = $group-1;
+                        echo("<h5>คุณอยู่กลุ่ม: {$group}</h5>");
+                    ?>
+                </div>
+            
+            </div>
+        
+        </div>
+
+    </div>
+
+    
+    <!-- Footer -->
+    <div>
+        Footer
+    </div>
+</body>
+</html>
--- student_dashboard.php
+++ student_dashboard.php
@@ -39,33 +39,39 @@
         $sql = "SELECT
                     student_score.class_id,
                     class.title,
-                    users.name
+                    users.name,
+                    result_grouped.classid
                 FROM
                     student_score
                 LEFT JOIN class ON class.id = student_score.class_id
                 JOIN users ON users.email = class.teacher_email
+                LEFT JOIN result_grouped ON result_grouped.classid = class.id
                 WHERE
                     student_score.std_email = '{$_SESSION['email']}'
                 GROUP BY
                     class_id";
         $result = mysqli_query($conn, $sql);
+
+        $nClass = mysqli_num_rows($result);
+        
         
     ?>
     <!-- content -->
     <div class="container-fluid">
         <div class="text-center">
-            <p><h3>DASHBOARD</h3></p>
+            <p><h3>STUDENT DASHBOARD</h3></p>
             <hr>
         </div>
 
         <div class="container">
-            <p>Class ที่ได้เข้าร่วม</p>
+            <p class="text-center"><?php echo("<b>Class ที่เข้ารวม :</b> <u>{$nClass}</u> <b>class</b>"); ?></p>
 
             <table class="table table-bordered table-striped  text-center">
                 <thead class="thead-dark">
-                    <th scope="col">Class ID</th>
-                    <th scope="col">Class Name</th>
-                    <th scope="col">Teacher</th>
+                    <th scope="col">รหัส Class</th>
+                    <th scope="col">ชื่อ Class</th>
+                    <th scope="col">อาจารย์เจ้าของ Class</th>
+                    <th scope="col">ผลลัพธ์การจัดกลุ่ม</th>
                 </thead>
                 <tbody>
                     <?php
@@ -75,6 +81,11 @@
                                 echo("<td>{$row['class_id']}</td>");
                                 echo("<td>{$row['title']}</td>");
                                 echo("<td>{$row['name']}</td>");
+                                if($row['classid'] == '') {
+                                    echo("<td><span class='btn btn-info disabled'>ยังไม่มีการจัดกลุ่ม</span></td>");
+                                } else {
+                                    echo("<td><a class='btn btn-primary' href='./showgrouped.php?c_id={$row['classid']}'>View</a></td>");
+                                }
                                 echo("</tr>");
                             }
                         }
