01a4d39b5d0f5bf49dc0b8033619b66fffe3048c
Kornchaval <kornchaval@gmail.com>
2018-12-03 14:22:14 +0700

    edit: teacher dashboard interface

--- teacher_dashboard.php
+++ teacher_dashboard.php
@@ -45,24 +45,29 @@
                 WHERE
                     class.teacher_email = '{$_SESSION['email']}'";
         $result = mysqli_query($conn, $sql);
+
+        $nClass = mysqli_num_rows($result);
         
     ?>
     <!-- content -->
     <div class="container-fluid">
         <div class="text-center">
-            <p><h3>DASHBOARD</h3></p>
+            <p><h3>TEACHER DASHBOARD</h3></p>
             <hr>
         </div>
 
-        <div class="container">
-            <p>Class ที่สร้าง</p>
+        
 
+
+        <div class="container">
+            <div class="text-center">
+                <p><b>จำนวน Class ที่สร้าง : </b><?php echo($nClass); ?> Class</p>
+            </div>
             <table class="table table-bordered table-striped  text-center">
                 <thead class="thead-dark">
-                    <th scope="col">Class ID</th>
-                    <th scope="col">Class Name</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">จำนวนผู้เข้าร่วม</th>
+                    <th scope="col" style="width: 30%">Class ID</th>
+                    <th scope="col" style="width: 40%">Class Name</th>
+                    <th scope="col" style="width: 30%">Class Manage</th>
                 </thead>
                 <tbody>
                     <?php
@@ -71,22 +76,21 @@
                                 echo("<tr>");
                                 echo("<td>{$row['id']}</td>");
                                 echo("<td>{$row['title']}</td>");
-                                echo("<td>{$row['description']}</td>");
-                                $sq = "SELECT class_id FROM student_score WHERE class_id = '{$row['id']}' ";
-                                $resultCount = mysqli_query($conn, $sq);
-                                $n = mysqli_num_rows($resultCount);
-                                echo("<td>{$n} คน</td>");
+                                echo("<td>
+                                        <a href='class_description.php?c_id={$row['id']}' class='btn btn-primary'>View</a>
+                                        <span class='btn btn-success'>Generate</span>
+                                        <a href='class_close.php?c_id={$row['id']}' class='btn btn-danger'>Close</a>
+                                    </td>");
                                 echo("</tr>");
                             }
                         }
+                       
+                                
+                           
                     ?>
-                    
-
                 </tbody>
-            
-            
+                
             </table>
-            
         </div>
 
     </div>
